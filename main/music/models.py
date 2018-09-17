ca918c7ca153e59dd1f88b88e81ead5834c30512
sa4dkhan <sa4dkhan@gmail.com>
2018-09-17 21:32:42 +0600

    update

--- main/music/models.py
+++ main/music/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Permission, User
 
 
 class Album(models.Model):
-    user = models.ForeignKey(User, default=1)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
     artist = models.CharField(max_length=255)
-
+    album_title = models.CharField(max_length=255)
+    genre = models.CharField()
